6c728786e70bd3eb0ac40c2ccd7a959b851ab4c1
Yuutsuy <kiraidase@gmail.com>
2022-04-21 18:24:56 -0300

    Adicionado novo input para receber dados de quantOuro, adicionado JavaScript para o botão exibirDados

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,3 +1,16 @@
-var chamar = function() {
-    alert('funfou');
+var quantPlanta = document.getElementById('quantPlantar');
+var quantOuro = document.getElementById('quantOuro');
+
+var exibirDados = function() {
+    if(quantOuro.value === "") {
+        alert("Planta");
+        alert(quantPlanta.value);
+        quantPlanta.value = "";
+        quantOuro.value = "";
+    }else{
+        alert("Ouro");
+        alert(quantOuro.value);
+        quantOuro.value = "";
+        quantPlanta.value = "";
+    }
 }

bd7f6efb064177b32532be38caeed3ff4228adeb
Yuutsuy <kiraidase@gmail.com>
2022-04-22 15:25:56 -0300

    adicionado box-shadow aos inputs e select da div primavera, adicionado box-shadow aos botões, adicionado calendário em uma div maisSobre

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,16 +1,22 @@
 var quantPlanta = document.getElementById('quantPlantar');
 var quantOuro = document.getElementById('quantOuro');
 
-var exibirDados = function() {
-    if(quantOuro.value === "") {
+var exibQuantidade = function() {
+    if(quantPlanta.value !== "") {
         alert("Planta");
         alert(quantPlanta.value);
         quantPlanta.value = "";
-        quantOuro.value = "";
     }else{
+        alert('Digite algo no campo de ouro');
+    }
+}
+
+var exibOuro = function() {
+    if(quantOuro.value !== "") {
         alert("Ouro");
         alert(quantOuro.value);
         quantOuro.value = "";
-        quantPlanta.value = "";
+    }else{
+        alert('Digite algo no campo de plantas');
     }
 }

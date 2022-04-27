3b8e940abc01538dbab05a0e26273ea60ad80e85
Yuutsuy <kiraidase@gmail.com>
2022-04-27 09:55:59 -0300

    Feito a alteração do elemento HTML h3 em Quantidade, criado objetos para utilização mais bem feita(feito até o obj couve)

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,99 +1,220 @@
-var quantPlanta = document.getElementById('quantPlantar');
-var quantOuro = document.getElementById('quantOuro');
-var opcao = document.getElementById('plantacao');
-var imgPlanta = document.getElementById('imgPlanta');
-var numQuant = document.getElementById('numQuantidade');
+// declarando elementos
+var elQuantPlanta = document.getElementById('quantPlantar');
+var elQuantOuro = document.getElementById('quantOuro');
+var elOpcao = document.getElementById('plantacao');
+var elImgPlanta = document.getElementById('imgPlanta');
+var elQuant = document.getElementById('numQuantidade');
+var elNumQuant = elQuant.children[1];
 
+
+// criando objetos
+var chirivia = {
+    "sementes" : 20,
+    "colheita" : 5,
+    "armazem" : "20 ouros",
+    "joja" : "25 ouros",
+    "carrinho" : "100 - 1000 ouros"
+};
+var alho = {
+    "sementes" : 20,
+    "colheita" : 5,
+    "armazem" : "40 ouros(a partir do segundo ano)",
+    "joja" : "não vendido",
+    "carrinho" : "100 - 1000 ouros"
+};
+var batata = {
+    "sementes" : 25,
+    "colheita" : 4,
+    "armazem" : "50 ouros",
+    "joja" : "62 ouros",
+    "carrinho" : "100 - 1000 ouros" 
+};
+var couve = {
+    "sementes" : 35,
+    "colheita" : 5,
+    "armazem" : "70 ouros",
+    "joja" : "87 ouros",
+    "carrinho" : "105 - 1000 ouros"
+};
+var couveFlor = {
+    "sementes" : 20,
+    "colheita" : 4
+};
+var ruibardo = {
+    "sementes" : 20,
+    "colheita" : 4
+};
+var vagem = {
+    "sementes" : 20,
+    "colheita" : 4
+};
+var cafe = {
+    "sementes" : 20,
+    "colheita" : 4
+};
+var morango = {
+    "sementes" : 20,
+    "colheita" : 4
+};
+var tulipa = {
+    "sementes" : 20,
+    "colheita" : 4
+};
+var jarmimAzul = {
+    "sementes" : 20,
+    "colheita" : 4
+};
+
+// chamando a função exibQuantidade
 var exibQuantidade = function() {
     // limpa a imagem na div imgPlanta
-    imgPlanta.classList = '';
+    elImgPlanta.classList = '';
+    // alert(elQuantPlanta.value);
     // cria condição para avaliar se a caixa de entrada recebe ou não algum valor
-    if(quantPlanta.value !== "") {
+    if(elQuantPlanta.value !== "") {
         // alert("Planta");
-        quantPlanta.value = ""; // limpa a caixa de entrada com o id quantPlanta
         // verifica o valor selecionado no id opcoes
-        switch(opcao.value) {
+        switch(elOpcao.value) {
             case 'opt1':
-                imgPlanta.classList.add('chirivia');
+                elImgPlanta.classList.add('chirivia');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt2':
-                imgPlanta.classList.add('alho');
+                elImgPlanta.classList.add('alho');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt3':
-                imgPlanta.classList.add('batata');
+                elImgPlanta.classList.add('batata');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt4':
-                imgPlanta.classList.add('couve');
+                elImgPlanta.classList.add('couve');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt5':
-                imgPlanta.classList.add('couve-flor');
+                elImgPlanta.classList.add('couve-flor');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt6':
-                imgPlanta.classList.add('ruibardo');
+                elImgPlanta.classList.add('ruibardo');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt7':
-                imgPlanta.classList.add('vagem');
+                elImgPlanta.classList.add('vagem');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt8':
-                imgPlanta.classList.add('cafe');
+                elImgPlanta.classList.add('cafe');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt9':
-                imgPlanta.classList.add('morango');
+                elImgPlanta.classList.add('morango');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt10':
-                imgPlanta.classList.add('tulipa');
+                elImgPlanta.classList.add('tulipa');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
             case 'opt11':
-                imgPlanta.classList.add('jasmim-azul');
+                elImgPlanta.classList.add('jasmim-azul');
+                if(elQuantPlanta.value <= 9){
+                    elNumQuant.innerHTML = `0${elQuantPlanta.value}`;
+                }else{
+                    elNumQuant.innerHTML = elQuantPlanta.value;
+                }
                 break;
+            default:
+                alert('Isso não é uma opção');
         };
+        elQuantPlanta.value = ""; // limpa a caixa de entrada com o id quantPlanta
     }else{
         alert('Digite algo no campo de quantidade de plantas');
     }
 }
 
+// chamando a função exibOuro
 var exibOuro = function() {
     // limpa a imagem na div imgPlanta
-    imgPlanta.classList = '';
+    elImgPlanta.classList = '';
     // cria condição para avaliar se a caixa de entrada recebe ou não algum valor
-    if(quantOuro.value !== "") {
+    if(elQuantOuro.value !== "") {
         // alert("Ouro");
-        // alert(quantOuro.value);
-        quantOuro.value = ""; // limpa a caixa de entrada quantOuro
+        // alert(elQuantOuro.value);
+        elQuantOuro.value = ""; // limpa a caixa de entrada elQuantOuro
         // verifica o valor selecionado no id opcoes
-        switch(opcao.value) {
+        switch(elOpcao.value) {
             case 'opt1':
-                imgPlanta.classList.add('chirivia');
+                elImgPlanta.classList.add('chirivia');
                 break;
             case 'opt2':
-                imgPlanta.classList.add('alho');
+                elImgPlanta.classList.add('alho');
                 break;
             case 'opt3':
-                imgPlanta.classList.add('batata');
+                elImgPlanta.classList.add('batata');
                 break;
             case 'opt4':
-                imgPlanta.classList.add('couve');
+                elImgPlanta.classList.add('couve');
                 break;
             case 'opt5':
-                imgPlanta.classList.add('couve-flor');
+                elImgPlanta.classList.add('couve-flor');
                 break;
             case 'opt6':
-                imgPlanta.classList.add('ruibardo');
+                elImgPlanta.classList.add('ruibardo');
                 break;
             case 'opt7':
-                imgPlanta.classList.add('vagem');
+                elImgPlanta.classList.add('vagem');
                 break;
             case 'opt8':
-                imgPlanta.classList.add('cafe');
+                elImgPlanta.classList.add('cafe');
                 break;
             case 'opt9':
-                imgPlanta.classList.add('morango');
+                elImgPlanta.classList.add('morango');
                 break;
             case 'opt10':
-                imgPlanta.classList.add('tulipa');
+                elImgPlanta.classList.add('tulipa');
                 break;
             case 'opt11':
-                imgPlanta.classList.add('jasmim-azul');
+                elImgPlanta.classList.add('jasmim-azul');
                 break;
         };
     }else{
